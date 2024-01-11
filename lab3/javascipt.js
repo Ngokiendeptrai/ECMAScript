f32ff60a573fa803f6f9264b4f5ae85b121124d5
Ngokiendeptrai <129733051+Ngokiendeptrai@users.noreply.github.com>
2024-01-11 21:39:06 +0700

    Bài lab3 NgovantuankienPc07176

---
+++ lab3/javascipt.js
@@ -0,0 +1,32 @@
+
+// const hamso1 = (num1,num2) => num1 * num2;
+// const multiply = fahtenhenit => (5/9) * (fahtenhenit - 32);
+//
+// const  padZeros = (num,totaLlen) => {
+//     let numStr = num.toString();
+//     let numZeros = totaLlen - numStr.length;
+//     for (let i =1; i <= numZeros; i ++){
+//         numStr = "0" +numStr;
+//     }
+//     return numStr
+// };
+
+const sum = (num1,num2) => num1 + num2;
+
+console.log(sum(2,4))
+ const sum1 =1;
+
+let conmeocuatoi = (arr1,arr2) => {
+   "use strict";
+    let bacham = [...arr1,...arr2];
+ console.log(bacham)
+}
+conmeocuatoi([1,2],[3,4,5])
+
+let dog = {
+ name: "Hoang",
+ age: 18,
+
+}
+console.log(dog.name);
+
