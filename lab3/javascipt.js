f0a6f3f359a682db9787cd426d6b0a5b339864b8
Ngokiendeptrai <129733051+Ngokiendeptrai@users.noreply.github.com>
2024-01-16 18:35:50 +0700

    Bài Lab3 NgovantuankienPc07176

--- lab3/javascipt.js
+++ lab3/javascipt.js
@@ -1,32 +1,52 @@
+let multiply = (num1, num2) => num1 * num2;
 
-// const hamso1 = (num1,num2) => num1 * num2;
-// const multiply = fahtenhenit => (5/9) * (fahtenhenit - 32);
-//
-// const  padZeros = (num,totaLlen) => {
-//     let numStr = num.toString();
-//     let numZeros = totaLlen - numStr.length;
-//     for (let i =1; i <= numZeros; i ++){
-//         numStr = "0" +numStr;
-//     }
-//     return numStr
-// };
 
-const sum = (num1,num2) => num1 + num2;
+let toCelsius = (fahrenheit) => (5 / 9) * (fahrenheit - 32);
 
-console.log(sum(2,4))
- const sum1 =1;
 
-let conmeocuatoi = (arr1,arr2) => {
-   "use strict";
-    let bacham = [...arr1,...arr2];
- console.log(bacham)
-}
-conmeocuatoi([1,2],[3,4,5])
+let padZeros = (num, totalLen = 2) => num.toString().padStart(totalLen, "0");
 
-let dog = {
- name: "Hoang",
- age: 18,
 
-}
-console.log(dog.name);
+let power = (base, exponent) => base ** exponent;
 
+let greet = (who) => {
+ console.log("Hello" + who);
+};
+
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
+
+const sumArr = (arr) => arr.reduce((kt, gtm) => kt + gtm, 0);
+console.log(sumArr(arr));
+
+
+var Entity = function (name, delay) {
+ this.name = name;
+ this.delay = delay;
+};
+
+Entity.prototype.greet = function () {
+ setTimeout(() => {
+  console.log("Xin chào, tên tôi là", this.name);
+ }, this.delay);
+};
+
+var java = new Entity("java", 5000);
+var cpp = new Entity("C++", 30);
+
+java.greet();
+cpp.greet();
+
+
+const convertTemperature = (temperature, unit) => {
+ if (unit === "C") {
+  let Fahrenheit = (temperature * 9 / 5 ) + 32;
+  console.log("Độ", Fahrenheit);
+  return;
+ } else if (unit === "F") {
+  let Celsius = ((temperature - 32) * 5) / 9;
+  console.log("Độ", Celsius);
+ }
+};
+
+console.log(convertTemperature(20, "C"));
